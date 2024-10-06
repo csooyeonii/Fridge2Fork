10eb93b60eb1145873523e9cb0ec8bd3e4beed81
u245 <mikhail.tuev@tmvsystems.com>
2024-10-06 11:19:22 -0700

    Final version of the database.

--- test1.sql
+++ test1.sql
@@ -1,15 +1,150 @@
-DROP TABLE IF EXISTS `ingredient`;
+-- MySQL dump 10.13  Distrib 8.0.39, for Win64 (x86_64)
+--
+-- Host: localhost    Database: test1
+-- ------------------------------------------------------
+-- Server version	8.0.39
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `fridge`
+--
+
+DROP TABLE IF EXISTS `fridge`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fridge` (
+  `serialNumber` int NOT NULL,
+  PRIMARY KEY (`serialNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fridge`
+--
+
+LOCK TABLES `fridge` WRITE;
+/*!40000 ALTER TABLE `fridge` DISABLE KEYS */;
+INSERT INTO `fridge` VALUES (1),(2),(3);
+/*!40000 ALTER TABLE `fridge` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fridgeingredient`
+--
+
+DROP TABLE IF EXISTS `fridgeingredient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fridgeingredient` (
+  `name` varchar(255) NOT NULL,
+  `serialNumber` int NOT NULL,
+  PRIMARY KEY (`name`,`serialNumber`),
+  KEY `serialNumber` (`serialNumber`),
+  CONSTRAINT `fridgeingredient_ibfk_1` FOREIGN KEY (`name`) REFERENCES `ingredient` (`name`),
+  CONSTRAINT `fridgeingredient_ibfk_2` FOREIGN KEY (`serialNumber`) REFERENCES `fridge` (`serialNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fridgeingredient`
+--
+
+LOCK TABLES `fridgeingredient` WRITE;
+/*!40000 ALTER TABLE `fridgeingredient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fridgeingredient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredient`
+--
+
+DROP TABLE IF EXISTS `ingredient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ingredient` (
   `name` varchar(255) NOT NULL,
   `ingredientCondition` varchar(255) NOT NULL,
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredient`
+--
 
 LOCK TABLES `ingredient` WRITE;
-
-INSERT INTO `ingredient` VALUES ('Tomato','Good');
-INSERT INTO `ingredient` VALUES ('Cucumber','Good');
-INSERT INTO `ingredient` VALUES ('Banana','Good');
-
+/*!40000 ALTER TABLE `ingredient` DISABLE KEYS */;
+INSERT INTO `ingredient` VALUES ('Banana','Good'),('Cucumber','Good'),('Tomato','Good');
+/*!40000 ALTER TABLE `ingredient` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `recipe`
+--
+
+DROP TABLE IF EXISTS `recipe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `recipe` (
+  `recipeNumber` int NOT NULL,
+  `text` varchar(255) NOT NULL,
+  PRIMARY KEY (`recipeNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipe`
+--
+
+LOCK TABLES `recipe` WRITE;
+/*!40000 ALTER TABLE `recipe` DISABLE KEYS */;
+INSERT INTO `recipe` VALUES (1,'Recipe 1'),(2,'Recipe 2'),(3,'Recipe 3');
+/*!40000 ALTER TABLE `recipe` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recipeingredient`
+--
+
+DROP TABLE IF EXISTS `recipeingredient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `recipeingredient` (
+  `name` varchar(255) NOT NULL,
+  `recipeNumber` int NOT NULL,
+  PRIMARY KEY (`name`,`recipeNumber`),
+  KEY `recipeNumber` (`recipeNumber`),
+  CONSTRAINT `recipeingredient_ibfk_1` FOREIGN KEY (`name`) REFERENCES `ingredient` (`name`),
+  CONSTRAINT `recipeingredient_ibfk_2` FOREIGN KEY (`recipeNumber`) REFERENCES `recipe` (`recipeNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipeingredient`
+--
+
+LOCK TABLES `recipeingredient` WRITE;
+/*!40000 ALTER TABLE `recipeingredient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recipeingredient` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-06 11:17:22
